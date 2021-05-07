a040c3260a40cd52aae21d96a3f65e395760acc3
hudaisme <najihaazmi72@gmail.com>
2021-05-07 16:51:12 +0000

    commit for server

--- Binding.c
+++ Binding.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>	//strlen
 #include <sys/socket.h>
 #include <arpa/inet.h>	//inet_addr
+#include <unistd.h>	//write
 
 int main(int argc, char*argv[])
 {
 	int socket_desc, new_socket, c;
 	struct sockaddr_in server, client;
+	char*message;
 
 	//Create socket
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -14,7 +17,7 @@ int main(int argc, char*argv[])
 		printf("Could not create socket");
 	}
 
-	//Prepare the sockaddr_instructure
+	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons(8888);
@@ -23,6 +26,7 @@ int main(int argc, char*argv[])
 	if(bind(socket_desc, (struct sockaddr*)&server, sizeof(server)) <0)
 	{
 		puts("bind done");
+		return 1;
 	}
 
 	puts("bind done");
@@ -33,9 +37,20 @@ int main(int argc, char*argv[])
 	//Accept and incoming connection
 	puts("Waiting for incoming connections...");
 	c = sizeof(struct sockaddr_in);
-	new_socket = accept(socket_desc, (struct sockaddr*) &client, (socklen_t*) &c);
+
+	while( (new_socket = accept(socket_desc, (struct sockaddr*)&client, (socklen_t*)&c)) )
+	{
+		puts("Connection accepted");
+
+		//Reply to the client
+		message = "Hello Client, I have received your connection. But I have to go now, bye\n";
+		write(new_socket, message, strlen(message));
+	}
+
 	if(new_socket < 0)
 	{
-		perror("accept failed");
+		perror("Accept failed");
+		return 1;
 	}
+	return 0;
 }
